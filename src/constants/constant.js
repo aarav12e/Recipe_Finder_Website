1f829746dccaa38faefe2a2b49f92c68c34fdc35
AaRAv336 <aarav12f@gmail.com>
2026-01-18 16:57:40 +0530

    feat: Add logo asset and define API_URL constant.

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,5 @@
-export const logo = 'https://ourrecipes.in/wp-content/uploads/2021/09/our-recipes-logo-retina.png';
+import logoImage from '../assets/logo.png';
+export const logo = logoImage;
 
 
 export const API_URL = 'https://forkify-api.herokuapp.com/api';
