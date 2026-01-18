c74d4294cd921b1605054d0f6e7a17712c27effb
AaRAv336 <aarav12f@gmail.com>
2026-01-18 12:27:26 +0530

    Added Footer Section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import Recipes from './pages/Recipes';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
+import Footer from "./components/common/Footer";
+
 
 function App() {
   return (
@@ -14,7 +16,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+
       </Routes>
+       <Footer />
     </Router>
   );
 }
